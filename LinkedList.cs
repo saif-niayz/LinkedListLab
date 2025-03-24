15607b12cfd01c62cb612a9de0c2e032e6cb3e54
saif-niayz <niayz.saif@gmail.com>
2025-03-24 01:01:16 -0600

    Add files via upload

---
+++ LinkedList.cs
@@ -0,0 +1,83 @@
+﻿public class Node
+{
+    public string Value;
+    public Node Next;
+
+    public Node(string value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
+
+public class LinkedList
+{
+    public Node Head { get; private set; }
+    public int Count { get; private set; }
+
+    public void AddFirst(string value)
+    {
+        Node newNode = new Node(value)
+        {
+            Next = Head
+        };
+        Head = newNode;
+        Count++;
+    }
+
+    public void AddLast(string value)
+    {
+        Node newNode = new Node(value);
+        if (Head == null)
+        {
+            Head = newNode;
+        }
+        else
+        {
+            Node current = Head;
+            while (current.Next != null)
+                current = current.Next;
+            current.Next = newNode;
+        }
+        Count++;
+    }
+
+    public void RemoveFirst()
+    {
+        if (Head != null)
+        {
+            Head = Head.Next;
+            Count--;
+        }
+    }
+
+    public void RemoveLast()
+    {
+        if (Head == null) return;
+
+        if (Head.Next == null)
+        {
+            Head = null;
+        }
+        else
+        {
+            Node current = Head;
+            while (current.Next.Next != null)
+                current = current.Next;
+            current.Next = null;
+        }
+        Count--;
+    }
+
+    public string GetValue(int index)
+    {
+        if (index < 0 || index >= Count)
+            throw new IndexOutOfRangeException();
+
+        Node current = Head;
+        for (int i = 0; i < index; i++)
+            current = current.Next;
+
+        return current.Value;
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,98 @@
+﻿using Xunit;
+
+namespace LinkedListLab.Tests
+{
+    public class LinkedListTests
+    {
+        string[] names = new string[]
+        {
+            "Joe Blow",
+            "Joe Schmoe",
+            "John Smith",
+            "Jane Doe",
+            "Bob Bobberson",
+            "Sam Sammerson",
+            "Dave Daverson"
+        };
+
+        [Fact]
+        public void AddAllNames_AddLast_WorksCorrectly()
+        {
+            var list = new LinkedList();
+
+            foreach (var name in names)
+                list.AddLast(name);
+
+            Assert.Equal(7, list.Count);
+            for (int i = 0; i < names.Length; i++)
+                Assert.Equal(names[i], list.GetValue(i));
+        }
+
+        [Fact]
+        public void AddAllNames_AddFirst_WorksCorrectly()
+        {
+            var list = new LinkedList();
+
+            foreach (var name in names)
+                list.AddFirst(name);
+
+            Assert.Equal(7, list.Count);
+            for (int i = 0; i < names.Length; i++)
+                Assert.Equal(names[names.Length - 1 - i], list.GetValue(i));
+        }
+
+        [Fact]
+        public void RemoveFirst_RemovesCorrectNode()
+        {
+            var list = new LinkedList();
+            foreach (var name in names)
+                list.AddLast(name);
+
+            list.RemoveFirst(); // Removes "Joe Blow"
+
+            Assert.Equal(6, list.Count);
+            Assert.Equal("Joe Schmoe", list.GetValue(0));
+        }
+
+        [Fact]
+        public void RemoveLast_RemovesCorrectNode()
+        {
+            var list = new LinkedList();
+            foreach (var name in names)
+                list.AddLast(name);
+
+            list.RemoveLast(); // Removes "Dave Daverson"
+
+            Assert.Equal(6, list.Count);
+            Assert.Throws<IndexOutOfRangeException>(() => list.GetValue(6));
+        }
+
+        [Fact]
+        public void GetValue_ReturnsCorrectValue()
+        {
+            var list = new LinkedList();
+            foreach (var name in names)
+                list.AddLast(name);
+
+            Assert.Equal("Jane Doe", list.GetValue(3));
+        }
+
+        [Fact]
+        public void GetValue_InvalidIndex_ThrowsException()
+        {
+            var list = new LinkedList();
+            Assert.Throws<IndexOutOfRangeException>(() => list.GetValue(0));
+        }
+
+        [Fact]
+        public void RemoveFromEmptyList_DoesNotCrash()
+        {
+            var list = new LinkedList();
+            list.RemoveFirst();
+            list.RemoveLast();
+
+            Assert.Null(list.Head);
+            Assert.Equal(0, list.Count);
+        }
+    }
+}
